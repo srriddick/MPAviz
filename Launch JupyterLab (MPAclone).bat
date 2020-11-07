e2c18692b9e152c60c9d177edcf489be896c55bf
srriddick <sage.riddick@duke.edu>
2020-11-07 15:45:38 -0500

    jupyter

--- Launch JupyterLab (MPAclone).bat
+++ Launch JupyterLab (MPAclone).bat
@@ -1,4 +1,4 @@
 @set the_env=MPAclone
 call "C:\Program Files\ArcGIS\Pro\bin\Python\Scripts\activate.bat" "%the_env%"
-call "%localappdata%\ESRI\conda\envs\%the_env%\scripts\jupyter-lab.exe"
+call "%localappdata%\ESRI\conda\envs\%the_env%\scripts\jupyter-notebook.exe" %cd% 
 call "C:\Program Files\ArcGIS\Pro\bin\Python\Scripts\deactivate.bat"
